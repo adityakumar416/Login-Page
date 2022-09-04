fdaae966a123c3fc1630c3647bf688f68adf015a
macBook <Aditya0683624@gmail.com>
2022-09-04 22:59:17 +0530

    login page

---
+++ app/src/main/java/com/example/myapplication/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        Handler handler;
+        handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent=new Intent(SplashScreenActivity.this,MainActivity2.class);
+                startActivity(intent);
+                finish();
+            }
+        },1000);
+
+
+
+    }
+}
